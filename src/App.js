6375b81ef33c14c7c4d5e0f22056b15d8ec5b30a
akumbom <akumbom5ma@gmail.com>
2022-11-15 16:04:35 +0100

    wip: page header design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
+import './components/header/styles.scss'
+import './default.scss'
+import Header from './components/header/index.js'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <h1>Hello World</h1>
     </div>
   );
 }
